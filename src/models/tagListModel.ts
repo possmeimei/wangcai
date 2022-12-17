5e3b25bbc740ec217f321710ed133bf546a0bbfc
zhangchen <383922689@qq.com>
2022-12-17 20:56:51 +0800

    编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -7,18 +7,36 @@ type TagListModel = {
     data: Tag[]
     fetch: () => Tag[]
     create: (name: string) => 'success' | 'duplicated' //联合类型
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean
     save: () => void
 }
 const tagListModel: TagListModel = {
     data: [],
     create(name) {
-        const names = this.data.map(item => item.name)
+        const names = this.data.map(item => item.name);
         if (names.indexOf(name) >= 0) {
             return 'duplicated';
         }
-        this.data.push({id:name,name:name});
+        this.data.push({id: name, name: name});
         this.save();
         return 'success';
+    },
+    update(id, name) {
+        const idList = this.data.map(item => item.id);
+        if (idList.indexOf(id) >= 0) {
+            const names = this.data.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                return 'duplicated';
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0];
+                tag.name = name;
+                this.save;
+                return 'success';
+            }
+        } else {
+            return 'not found';
+        }
     },
     fetch() {
         this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
@@ -27,5 +45,17 @@ const tagListModel: TagListModel = {
     save() {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
     },
+    remove(id: string) {
+        let index = -1;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        this.data.splice(index, 1);
+        this.save();
+        return true;
+    }
 };
 export default tagListModel;
--- src/views/LabelsEdit.vue
+++ src/views/LabelsEdit.vue
@@ -1,15 +1,17 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="arrow-left" />
-     <span class="title">编辑标签</span>
+      <Icon class="leftIcon" name="arrow-left" @click="goBack"/>
+      <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <EditItem field-name="标签名" placeholder="请输入标签名"/>
+      <EditItem :value="tag.name"
+                @update:value="updateTag"
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="removeTag">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -20,26 +22,46 @@ import {Component} from 'vue-property-decorator';
 import TagListModel from '@/models/tagListModel';
 import EditItem from '@/components/Money/EditItem.vue';
 import Button from '@/components/Button.vue';
+import tagListModel from '@/models/tagListModel';
+
 @Component({
   components: {Button, EditItem}
 })
 export default class LabelsEdit extends Vue {
+  tag?: { id: string, name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     TagListModel.fetch();
-    const tags = TagListModel.data
-    const tag = tags.filter(tag => tag.id === id)[0]
-    if (tag){
-      console.log(tag);
-    }else {
-      this.$router.replace('/404')
+    const tags = TagListModel.data;
+    const tag = tags.filter(tag => tag.id === id)[0];
+    if (tag) {
+      this.tag = tag;
+    } else {
+      this.$router.replace('/404');
     }
+  }
+
+  updateTag(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  removeTag() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
+    }
+  }
+
+  goBack() {
+    this.$router.back();
   }
 }
 </script>
 
 <style scoped lang="scss">
-.navBar{
+.navBar {
   text-align: center;
   font-size: 16px;
   padding: 12px 16px;
@@ -47,19 +69,23 @@ export default class LabelsEdit extends Vue {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  >.leftIcon{
+
+  > .leftIcon {
     width: 1em;
     height: 1em;
   }
-  >.title{
+
+  > .title {
 
   }
-  >.rightIcon{
+
+  > .rightIcon {
     width: 1em;
     height: 1em;
   }
 }
-.form-wrapper{
+
+.form-wrapper {
   background: white;
   margin-top: 6px;
   height: 44px;
@@ -67,7 +93,8 @@ export default class LabelsEdit extends Vue {
   justify-content: left;
   align-items: center;
 }
-.button-wrapper{
+
+.button-wrapper {
   text-align: center;
 }
 </style>
