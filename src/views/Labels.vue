e9aeda4ad9fa773050dc4d78b08f26d05e1b180c
zhangchen <383922689@qq.com>
2022-12-23 12:18:30 +0800

    Labels.vue 使用 vuex

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -20,18 +20,23 @@ import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 
 @Component({
-  components: {Button}
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    }
+  }
 })
 export default class Labels extends Vue {
-  // TODO
-  // tags = store.tagList;
-    tags = []
+  created() {
+    this.$store.commit('fetchTags');
+  }
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (name) {
-      // TODO
-     // store.createTag(name)
+    if (!name) {
+      return window.alert('标签名不能为空');
     }
+    this.$store.commit('createTag', name);
   }
 }
 </script>
